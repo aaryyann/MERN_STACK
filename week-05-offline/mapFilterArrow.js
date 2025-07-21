97f54a4ccd72a892c98b6d8fe3167c81be3b38d8
aaryyann <arya000045@gmail.com>
2025-07-21 12:35:28 +0530

    map, filter uses files added

--- week-05-offline/mapFilterArrow.js
+++ week-05-offline/mapFilterArrow.js
@@ -10,18 +10,20 @@
 
 // Problem Statement :- Given an array , give me back a new array in which every value is multiplied by 2
 
-const numbers = [1,2,3,4,5]
+const numbers = [1, 2, 3, 4, 5]
 
 const doubledNumbers = numbers.map(num => num * 2)
 
 console.log(doubledNumbers) // [2, 4, 6, 8, 10]
 
 // Filtering
+// Uses
 
 // what if I tell you , given an input array , give me backa all even values
 
-const li = [1 , 2 ,3 ,4 ,5 , 6 , 10 , 33, 44]
+const li = [1, 2, 3, 4, 5, 6, 10, 33, 44]
 
-const evenArr = li.filter(li => li%2 == 0)
+const evenArr = li.filter(li => li % 2 == 0)
 
 console.log(evenArr)
+console.log("End")
